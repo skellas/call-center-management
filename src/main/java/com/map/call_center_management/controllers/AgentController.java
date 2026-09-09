c540f3146f42191b3a9c77ce2afd0fa3bb84d67f
Seth Kellas <sjkellas@gmail.com>
2026-09-09 15:01:23 -0400

    Updating the AgentController for added functionality

--- src/main/java/com/map/call_center_management/controllers/AgentController.java
+++ src/main/java/com/map/call_center_management/controllers/AgentController.java
@@ -1,11 +1,17 @@
 package com.map.call_center_management.controllers;
 
+import static com.map.call_center_management.data.enums.AgentAvailability.OFFLINE;
+import static java.lang.Boolean.FALSE;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,11 +31,43 @@ public class AgentController {
 		return agentRepository.findAll();
 	}
 	
+	@GetMapping(value = "{agentId}")
+    public ResponseEntity<Agent> getAgentById(@PathVariable(name = "agentId") Long agentId) {
+		return ResponseEntity.ok(agentRepository.findById(agentId).get());
+	}
+
 	@PostMapping
 	public ResponseEntity<Agent> addAgent(@RequestBody Agent agent){ 
-		Agent persistedAgent = agentRepository.save(agent);
-		
-		return ResponseEntity.ok(persistedAgent);
+		return ResponseEntity.ok(agentRepository.save(agent));
 	}
+
+	@PutMapping(value = "/{agentId}")
+    public ResponseEntity<Agent> updateAgent(@PathVariable(name = "agentId") Long agentId, @RequestBody Agent agentInfo) {
+		Agent agent = agentRepository.findById(agentId).get();
+		
+		return ResponseEntity.ok(
+				agentRepository.save(
+						agent.toBuilder()
+							.name(agentInfo.getName())
+							.phoneNumber(agentInfo.getPhoneNumber())
+							.availability(agentInfo.getAvailability())
+							.build()
+							)
+				);
+	}
+	
+	@DeleteMapping(value = "/{agentId}")
+    public ResponseEntity<Agent> decommissionAgent(@PathVariable(name = "agentId") Long agentId) {
+		Agent agent = agentRepository.findById(agentId).get();
+		return ResponseEntity.ok(
+				agentRepository.save(
+						agent.toBuilder()
+							.active(FALSE)
+							.availability(OFFLINE)
+							.build()
+							)
+				);
+	}
+
 
 }
--- src/test/java/com/map/call_center_management/controllers/AgentControllerTest.java
+++ src/test/java/com/map/call_center_management/controllers/AgentControllerTest.java
@@ -1,11 +1,20 @@
 package com.map.call_center_management.controllers;
 
+import static com.map.call_center_management.data.enums.AgentAvailability.BUSY;
+import static java.lang.Boolean.FALSE;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +24,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.map.call_center_management.data.entities.Agent;
 import com.map.call_center_management.data.repositories.AgentRepository;
+
+import tools.jackson.databind.ObjectMapper;
 
 
 @WebMvcTest(AgentController.class)
@@ -42,5 +53,91 @@ class AgentControllerTest {
 				.andExpect(jsonPath("$[1].id").value(2L));
 
 	}
+
+	@Test
+	void shouldReturnAgentById() throws Exception {
+		// Given
+		Agent agent = Agent.builder().name("Jane Doe").phoneNumber("123-456-7890").id(1L).build();
+		
+		when(repo.findById(agent.getId())).thenReturn(Optional.of(agent));
+
+		// When / Then
+		mockMvc.perform(
+					get(String.format("/agents/%d", agent.getId()))
+				)
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("id").value(agent.getId()));
+
+	}
+
+	@Test
+	void shouldCreateAgent() throws Exception {
+		// Given
+		Agent postedAgent = Agent.builder().name("Jane Doe").phoneNumber("123-456-7890").build();
+		Agent persistedAgent = postedAgent.toBuilder().id(1L).build();
+		
+		when(repo.save(postedAgent)).thenReturn(persistedAgent);
+
+		// When / Then
+		mockMvc.perform(
+					post("/agents")
+					.content(asJsonString(postedAgent))
+					.contentType(APPLICATION_JSON)
+				)
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("id").value(persistedAgent.getId()));
+
+	}
+	
+
+	@Test
+	void shouldSaveAgent() throws Exception {
+		// Given
+		Agent persistedAgent = Agent.builder().name("Jane Doe").phoneNumber("123-456-7890").id(1L).build();
+		Agent updatedAgent = persistedAgent.toBuilder().availability(BUSY).build();
+		
+		when(repo.findById(persistedAgent.getId())).thenReturn(Optional.of(persistedAgent));
+		when(repo.save(any(Agent.class))).thenAnswer(returnsFirstArg());
+
+		// When / Then
+		mockMvc.perform(
+					put(String.format("/agents/%d", persistedAgent.getId()))
+					.content(asJsonString(updatedAgent))
+					.contentType(APPLICATION_JSON)
+				)
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("id").value(persistedAgent.getId()))
+				.andExpect(jsonPath("availability").value("BUSY"));
+
+	}
+	
+ 
+	@Test
+	void shouldDecommissionAgent() throws Exception {
+		// Given
+		Agent agent = Agent.builder().name("Jane Doe").phoneNumber("123-456-7890").id(1L).build();
+
+		when(repo.findById(agent.getId())).thenReturn(Optional.of(agent));
+		when(repo.save(any(Agent.class))).thenAnswer(returnsFirstArg());
+
+		// When / Then
+		mockMvc.perform(
+					delete(String.format("/agents/%d", agent.getId()))
+				)
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("id").value(agent.getId()))
+				.andExpect(jsonPath("active").value(FALSE));
+
+	}
+	
+	
+	private static String asJsonString(final Object obj) {
+
+		  try {
+		      return new ObjectMapper().writeValueAsString(obj);
+		  } catch (Exception e) {
+		      throw new RuntimeException(e);
+		  }
+		}
 
 }
