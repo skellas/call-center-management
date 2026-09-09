57ff0b81ff2a315dce02b43ddc2725445901c0f8
Seth Kellas <sjkellas@gmail.com>
2026-09-08 20:00:48 -0400

    Pushing initial maven structure

---
+++ src/main/java/com/map/call_center_management/CallCenterManagementApplication.java
@@ -0,0 +1,13 @@
+package com.map.call_center_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallCenterManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallCenterManagementApplication.class, args);
+	}
+
+}
